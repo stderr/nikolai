e8f6d5af9e759fd176b60b5bd011d9322d1a4b1d
stderr <std3rr@gmail.com>
2009-05-20 15:06:06 -0700

    A wee bit of reorganizing - 'Data' instances should be passed to the commands

--- bot.py
+++ bot.py
@@ -1,37 +1,8 @@
 import socket
-import settings
 import re
+import settings
+from tokenizer import Data
 
-class Data:
-    def __init__(self, raw_data):
-        self.raw = raw_data
-
-        if re.search('^:(.+)!(.+)@(.+) (.+) (.+) :(.+)$', raw_data):
-            m = re.search('^:(.+)!(.+)@(.+) (.+) (.+) :(.+)$', raw_data)
-            self.nick = m.group(1)
-            self.username = m.group(2)
-            self.host = m.group(3)
-            self.type = m.group(4)
-            self.channel = m.group(5)
-            self.message = m.group(6)
-        
-        elif re.search('^PING :(.*)$', raw_data):
-            m = re.search('^PING :(.*)$', raw_data)
-            self.host = m.group(1)
-            self.type = 'PING'
-
-        elif re.search('^:(.+) ([0-9]+) (.+)$', raw_data):
-            m = re.search('^:(.+) ([0-9]+) (.+)$', raw_data)
-            self.host = m.group(1)
-            self.type = 'SERVMSG'
-            self.code = m.group(2)
-            self.message = m.group(3)
-
-        else:
-            self.type = 'OTHER'
-
-    def __repr__(self):
-        return self.raw
 
 
 class Bot(object):
@@ -68,7 +39,7 @@ class Bot(object):
 
             if data.type == "PRIVMSG":
                for command in self._get_commands():
-                   command(self, data.message)
+                   command(self, data)
             
             elif data.type == "PING":
                 self._send("PONG")
--- tokenizer.py
+++ tokenizer.py
@@ -1,11 +1,31 @@
-class Line(object):
-    
-    def __init__(self, **args):
-        pass
+class Data(object):
+    def __init__(self, raw_data):
+        self.raw = raw_data
 
-
-class Tokenizer(object):
-
-    def __init__(self, **args):
-        pass
+        if re.search('^:(.+)!(.+)@(.+) (.+) (.+) :(.+)$', raw_data):
+            m = re.search('^:(.+)!(.+)@(.+) (.+) (.+) :(.+)$', raw_data)
+            self.nick = m.group(1)
+            self.username = m.group(2)
+            self.host = m.group(3)
+            self.type = m.group(4)
+            self.channel = m.group(5)
+            self.message = m.group(6)
         
+        elif re.search('^PING :(.*)$', raw_data):
+            m = re.search('^PING :(.*)$', raw_data)
+            self.host = m.group(1)
+            self.type = 'PING'
+
+        elif re.search('^:(.+) ([0-9]+) (.+)$', raw_data):
+            m = re.search('^:(.+) ([0-9]+) (.+)$', raw_data)
+            self.host = m.group(1)
+            self.type = 'SERVMSG'
+            self.code = m.group(2)
+            self.message = m.group(3)
+
+        else:
+            self.type = 'OTHER'
+
+    def __repr__(self):
+        return self.raw
+
